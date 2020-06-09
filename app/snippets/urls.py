d068b05d5c7552333b3981ef715da8bf3cce23c1
Jeon Young Hoon <hungyb0924@gmail.com>
2020-06-09 10:42:34 +0900

    user serializer, user APIView, snippet과의 relation 생성

--- app/snippets/urls.py
+++ app/snippets/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     # path('snippets/', views.snippet_list),
     # path('snippets/<int:pk>/', views.snippet_detail),
     path('', include(router.urls)),
+    path('users/', views.UserList.as_view()),
+    path('users/<int:pk>/', views.UserDetail.as_view()),
 ]
--- app/snippets/views.py
+++ app/snippets/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import permissions, viewsets
-
+from rest_framework import permissions, viewsets, generics
+from django.contrib.auth.models import User
 from snippets.models import Snippet
 # from snippets.permissions import IsOwnerOrReadOnly
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
 
 
 class SnippetViewSet(viewsets.ModelViewSet):
@@ -19,5 +19,18 @@ class SnippetViewSet(viewsets.ModelViewSet):
     serializer_class = SnippetSerializer
     permission_classes = [
         # permissions.IsAuthenticatedOrReadOnly,
-                          # IsOwnerOrReadOnly,
-                          ]
+        # IsOwnerOrReadOnly,
+    ]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
